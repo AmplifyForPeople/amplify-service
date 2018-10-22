db7bfa3e490c29d486a836eb38ec60364291fc42
Arnau Díaz <diazarnau0@gmail.com>
2018-10-22 21:33:51 +0200

    Spring added

---
+++ src/main/java/com/amplify/webservice/Controller.java
@@ -0,0 +1,14 @@
+package com.amplify.webservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "xd";
+    }
+
+}
